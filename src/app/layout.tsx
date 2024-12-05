35198c5ef0728c59d33217025aec5dc9bf42566b
jake <pjake2216@gmail.com>
2024-12-05 18:59:23 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./globals.css"
-;
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
